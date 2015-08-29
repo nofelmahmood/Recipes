bb0cc72fa58733658aa455c525b9e2f077f7d3c6
nofelmahmood <nofelmehmood@gmail.com>
2015-08-29 18:38:27 +0500

    RecipeStore added

---
+++ Recipes/RecipeStore.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeStore.swift
+//  Recipes
+//
+//  Created by Nofel Mahmood on 29/08/2015.
+//  Copyright © 2015 Hyper. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class RecipeStore: NSIncrementalStore {
+
+}
