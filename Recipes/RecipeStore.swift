debf240b449761ec4e0bba38c1ab5dae8932f2da
nofelmahmood <nofelmehmood@gmail.com>
2015-09-05 17:41:21 +0500

    Implementing save request in RecipeStore

--- Recipes/RecipeStore.swift
+++ Recipes/RecipeStore.swift
@@ -27,6 +27,9 @@ enum RecipeStoreError: ErrorType {
   case InvalidEntity
 }
 
+let RecipeStoreDidSaveSuccessfulNotification = "RecipeStoreDidSaveSuccessfulNotification"
+let RecipeStoreDidSaveUnSuccessfulNotification = "RecipeStoreDidSaveUnSuccessfulNotification"
+
 class RecipeStore: NSIncrementalStore
 {
   var cache = [NSNumber: [String: AnyObject]]()
@@ -62,13 +65,22 @@ class RecipeStore: NSIncrementalStore
   func executeInResponseToFetchRequest(request: NSFetchRequest, withContext context: NSManagedObjectContext) -> [NSManagedObject] {
     
     if let entityName = request.entityName where entityName == RecipeEntityName {
-      if let recipesKeyValues = RecipeApi.fetchAllRecipes() {
+      if let recipesKeyValues = RecipeApi.sharedAPI.recipes() {
         var fetchedObjects = [NSManagedObject]()
         let transformer = RecipeKeyValueTransformer(recipeEntity: request.entity!)
         for recipeKeyValue in recipesKeyValues {
           if let id = recipeKeyValue[RecipeEntityIDAttributeName] as? NSNumber {
             if let transformedValue = transformer.transformedValue(recipeKeyValue) as? [String: NSObject] {
               self.cache[id] = transformedValue
+              print("ORIGINALVALUE")
+              print(recipeKeyValue)
+              print("END")
+              print("TRANSFORMEDVALUE")
+              print(transformedValue)
+              print("END")
+              print("REVERSETRANSFORMEDVALUE")
+              print(transformer.reverseTransformedValue(transformedValue))
+              print("END")
               let managedObjectID = self.newObjectIDForEntity(request.entity!, referenceObject: id)
               let managedObject = context.objectWithID(managedObjectID)
               fetchedObjects.append(managedObject)
@@ -83,11 +95,56 @@ class RecipeStore: NSIncrementalStore
   
   func executeInResponseToSaveChangesRequest(request: NSSaveChangesRequest, withContext context: NSManagedObjectContext) -> [NSManagedObjectContext] {
     
-    if let deletedObjects = request.deletedObjects {
-      for deletedObject in deletedObjects {
-        if let recipeID = (deletedObject as? Recipe)?.id?.integerValue {
-          RecipeApi.deleteRecipe(recipeID, completionBlock: { (error) -> Void in
-            print(error)
+    if let recipeEntity = context.persistentStoreCoordinator?.managedObjectModel.entitiesByName[RecipeEntityName] {
+      let transformer = RecipeKeyValueTransformer(recipeEntity: recipeEntity)
+      let insertedRecipe = request.insertedObjects?.filter {
+        return $0 is Recipe
+      }.first as? Recipe
+      let deletedRecipe = request.deletedObjects?.filter {
+        return $0 is Recipe
+      }.first as? Recipe
+      let updatedRecipe = request.updatedObjects?.filter {
+        return $0 is Recipe
+      }.first as? Recipe
+      
+      if let insertedRecipe = insertedRecipe {
+        if let recipeParameters = transformer.reverseTransformedValue(insertedRecipe.dictionaryWithValuesForKeys(Array(insertedRecipe.entity.propertiesByName.keys))) as? [String: AnyObject] {
+          RecipeApi.sharedAPI.createOrUpdate(withRecipeID: nil, usingRecipeParameters: recipeParameters, photoData: nil, completionBlock: { (error) -> Void in
+            if error == nil {
+              NSOperationQueue.mainQueue().addOperationWithBlock {
+                NSNotificationCenter.defaultCenter().postNotificationName(RecipeStoreDidSaveSuccessfulNotification, object: context)
+              }
+            } else {
+              NSOperationQueue.mainQueue().addOperationWithBlock {
+                NSNotificationCenter.defaultCenter().postNotificationName(RecipeStoreDidSaveUnSuccessfulNotification, object: context)
+              }
+            }
+          })
+        }
+      } else if let deletedRecipeID = deletedRecipe?.id?.integerValue {
+        RecipeApi.sharedAPI.delete(deletedRecipeID, completionBlock: { (error) -> Void in
+          if error == nil {
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+              NSNotificationCenter.defaultCenter().postNotificationName(RecipeStoreDidSaveSuccessfulNotification, object: context)
+            }
+          } else {
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+              NSNotificationCenter.defaultCenter().postNotificationName(RecipeStoreDidSaveUnSuccessfulNotification, object: context)
+            }
+          }
+        })
+      } else if let updatedRecipe = updatedRecipe {
+        if let recipeID = updatedRecipe.id?.integerValue, let recipeParameters = transformer.reverseTransformedValue(updatedRecipe.dictionaryWithValuesForKeys(Array(updatedRecipe.entity.propertiesByName.keys))) as? [String: AnyObject] {
+          RecipeApi.sharedAPI.createOrUpdate(withRecipeID: recipeID, usingRecipeParameters: recipeParameters, photoData: nil, completionBlock: { (error) -> Void in
+            if error == nil {
+              NSOperationQueue.mainQueue().addOperationWithBlock {
+                NSNotificationCenter.defaultCenter().postNotificationName(RecipeStoreDidSaveSuccessfulNotification, object: context)
+              }
+            } else {
+              NSOperationQueue.mainQueue().addOperationWithBlock {
+                NSNotificationCenter.defaultCenter().postNotificationName(RecipeStoreDidSaveUnSuccessfulNotification, object: context)
+              }
+            }
           })
         }
       }
