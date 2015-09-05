aee63e3dbdad202f5066bf07f127f4841a90f5ff
nofelmahmood <nofelmehmood@gmail.com>
2015-09-05 17:49:03 +0500

    Adding Recipes workspace created by pod

---
+++ Recipes/Recipes-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Recipes-Bridging-Header.h
+//  Recipes
+//
+//  Created by Nofel Mahmood on 05/09/2015.
+//  Copyright © 2015 Hyper. All rights reserved.
+//
+
+#ifndef Recipes_Bridging_Header_h
+#define Recipes_Bridging_Header_h
+#import <AFNetworking/AFNetworking.h>
+
+
+#endif /* Recipes_Bridging_Header_h */
