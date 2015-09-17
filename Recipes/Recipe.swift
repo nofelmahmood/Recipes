55fa875fd749ac17e296a1cf1e1a5627a33d70a5
nofelmahmood <nofelmehmood@gmail.com>
2015-09-17 15:28:16 +0500

    Deleting unneeded files from first iteration

--- Recipes/Recipe.swift
+++ Recipes/Recipe.swift
@@ -1,172 +1,52 @@
 //
-//  RecipeModel.swift
+//  Recipe.swift
 //  Recipes
 //
-//  Created by Nofel Mahmood on 07/09/2015.
+//  Created by Nofel Mahmood on 15/09/2015.
 //  Copyright © 2015 Hyper. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import CoreData
 
-enum RecipeDifficulty: Int {
-  case Easy = 1
-  case Medium = 2
-  case Hard = 3
-}
+class Recipe: NSManagedObject {
 
-class Recipe: NSObject {
-  
-  var difficulty: NSNumber = NSNumber(integer: 1) {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  var favorite: Bool? {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  var id: NSNumber? {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  var name: String {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  var specification: String? {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  var instructions: [String]? {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  var photoURL: String? {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  var photoThumbnailURL: String? {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  var photoData: NSData? {
-    didSet {
-      self.hasChanges = true
-    }
-  }
-  
-  private var hasChanges = false
-  private var fromRemote: Bool {
-    if self.id != nil {
-      return true
-    }
-    return false
-  }
-  
-  private var serverRepresentation: [String: AnyObject] {
-    var representation = [RecipeKey.Name.rawValue: self.name, RecipeKey.Difficulty.rawValue: self.difficulty]
-    if let favorite = favorite {
-      representation[RecipeKey.Favorite.rawValue] = favorite
-    }
-    if let description = self.specification {
-      representation[RecipeKey.Description.rawValue] = description
-    }
-    if let instructions = self.instructions?.joinWithSeparator(",") {
-      representation[RecipeKey.Instructions.rawValue] = instructions
-    }
-    
-    return [RecipeKey.Recipe.rawValue: representation]
-  }
-  
-  init(fillFromRemoteKeyValue keyValue:[String: AnyObject]) {
-    self.name = ""
-    super.init()
-    self.fill(keyValue)
-    self.hasChanges = false
-  }
-  
-  override init() {
-    self.name = ""
-    super.init()
-    self.hasChanges = false
-  }
-
-  func changed() -> Bool {
-    return self.hasChanges
-  }
-  
-  func difficultyDescription() -> String? {
-    switch(difficulty) {
-    case RecipeDifficulty.Easy.rawValue:
-      return "\(RecipeDifficulty.Easy)"
-    case RecipeDifficulty.Medium.rawValue:
-      return "\(RecipeDifficulty.Medium)"
-    case RecipeDifficulty.Hard.rawValue:
-      return "\(RecipeDifficulty.Hard)"
-    default: break
+// Insert code here to add functionality to your managed object subclass
+  class func insertNewRecipe(usingRecipeApiModel recipeApiModel: RecipeApiModel, inContext context: NSManagedObjectContext) -> Recipe? {
+    if let recipe = NSEntityDescription.insertNewObjectForEntityForName("Recipe", inManagedObjectContext: context) as? Recipe {
+      recipe.id = recipeApiModel.id
+      recipe.name = recipeApiModel.name
+      recipe.difficulty = recipeApiModel.difficulty
+      recipe.instructions = recipeApiModel.instructions
+      recipe.favorite = recipeApiModel.favorite
+      recipe.createdAt = recipeApiModel.created_at
+      recipe.updatedAt = recipeApiModel.updated_at
+      recipe.specification = recipeApiModel.specification
+      recipe.photo = recipeApiModel.photoData
+      recipe.photoURL = recipeApiModel.url
+      recipe.photoThumbnailURL = recipeApiModel.thumbnail_url
+      return recipe
     }
     return nil
   }
   
-  func addInstruction(name: String){
-    if self.instructions != nil {
-      self.instructions?.append(name)
-    } else {
-      self.instructions = [name]
+  class func recipesWithIDs(IDs: [NSNumber], inContext context: NSManagedObjectContext) -> [Recipe]? {
+    let fetchRequest = NSFetchRequest(entityName: "Recipe")
+    fetchRequest.predicate = NSPredicate(format: "id IN %@", IDs)
+    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
+    let result = try? context.executeFetchRequest(fetchRequest)
+    if let result = result as? [Recipe] {
+      return result
     }
+    return nil
   }
   
-  private func fill(keyValue: [String: AnyObject]) {
-    if let id = keyValue[RecipeKey.ID.rawValue] as? NSNumber {
-      self.id = id
+  class func allRecipes(inContext context: NSManagedObjectContext) -> [Recipe]? {
+    let fetchRequest = NSFetchRequest(entityName: "Recipe")
+    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "updatedAt", ascending: false),NSSortDescriptor(key: "createdAt", ascending: false)]
+    if let result = try? context.executeFetchRequest(fetchRequest) as? [Recipe] {
+      return result
     }
-    if let name = keyValue[RecipeKey.Name.rawValue] as? String {
-      self.name = name
-    }
-    if let description = keyValue[RecipeKey.Description.rawValue] as? String {
-      self.specification = description
-    }
-    if let instructions = (keyValue[RecipeKey.Instructions.rawValue] as? String)?.componentsSeparatedByString(",") {
-      self.instructions = instructions
-    }
-    if let favorite = (keyValue[RecipeKey.Favorite.rawValue] as? NSNumber)?.boolValue {
-      self.favorite = favorite
-    }
-    if let difficulty = keyValue[RecipeKey.Difficulty.rawValue] as? NSNumber {
-      self.difficulty = difficulty
-    }
-    if let photo = keyValue[PhotoKey.Photo.rawValue] as? [String: String] {
-      if let photoURL = photo[PhotoKey.URL.rawValue] {
-        self.photoURL = photoURL
-      }
-      if let photoThumbnailURL = photo[PhotoKey.ThumbnailURL.rawValue] {
-        self.photoThumbnailURL = photoThumbnailURL
-      }
-    }
-  }
-  
-  func createOrUpdateOnRemote(completionBlock: ((successful: Bool)-> Void)?) {
-    RecipeApi.sharedAPI.createOrUpdate(withRecipeID: self.id?.integerValue, usingRecipeParameters: self.serverRepresentation, photoData: self.photoData) { (successful) -> Void in
-      completionBlock?(successful: successful)
-    }
-  }
-  
-  func deleteFromRemote(completionBlock: ((successful: Bool) -> Void)?) {
-    if let id = self.id?.integerValue {
-      RecipeApi.sharedAPI.delete(id, completionBlock: { (error) -> Void in
-        if error == nil {
-          completionBlock?(successful: true)
-        } else {
-          completionBlock?(successful: false)
-        }
-      })
-    }
+    return nil
   }
 }
