7fc135fae77892e428378d08e5ca5323003a4d73
nofelmahmood <nofelmehmood@gmail.com>
2015-09-28 17:13:53 +0500

    Updating Recipe Entity

--- Recipes/Recipe+CoreDataProperties.swift
+++ Recipes/Recipe+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Recipe+CoreDataProperties.swift
 //  Recipes
 //
-//  Created by Nofel Mahmood on 15/09/2015.
+//  Created by Nofel Mahmood on 24/09/2015.
 //  Copyright © 2015 Hyper. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -26,5 +26,7 @@ extension Recipe {
     @NSManaged var photoURL: String?
     @NSManaged var specification: String?
     @NSManaged var updatedAt: NSDate?
+    @NSManaged var changedKeys: String?
+    @NSManaged var isNew: NSNumber?
 
 }
