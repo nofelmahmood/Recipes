2cdd3adf47f85d1d94b2eb20b3eba6d60ddb5d91
nofelmahmood <nofelmehmood@gmail.com>
2015-09-30 13:27:22 +0500

    Adding RecipesListViewController collectionview UIScrollViewDelegate

---
+++ Recipes/RecipesListViewController+UIScrollView.swift
@@ -0,0 +1,28 @@
+//
+//  RecipesListViewController+UIScrollView.swift
+//  Recipes
+//
+//  Created by Nofel Mahmood on 30/09/2015.
+//  Copyright © 2015 Hyper. All rights reserved.
+//
+
+import UIKit
+
+extension RecipesListViewController: UIScrollViewDelegate {
+  func scrollViewDidScroll(scrollView: UIScrollView) {
+    let visibleCells = self.collectionView.visibleCells()
+    for cell in visibleCells {
+      if (cell as? UICollectionViewCellParallax) != nil {
+        let bounds = self.collectionView.bounds
+        let boundsCenter = CGPoint(x: CGRectGetMidX(bounds), y: CGRectGetMidY(bounds))
+        let cellCenter = cell.center
+        let offsetFromCenter = CGPointMake(boundsCenter.x - cellCenter.x, boundsCenter.y - cellCenter.y)
+        let cellSize = cell.bounds.size
+        let maxVerticalOffset = (bounds.size.height / 2) + (cellSize.height / 2)
+        let scaleFactor = 18.0 / maxVerticalOffset
+        let parallaxOffset = CGPointMake(0.0, -offsetFromCenter.y * scaleFactor)
+        (cell as? UICollectionViewCellParallax)?.updateWithParallaxOffset(parallaxOffset)
+      }
+    }
+  }
+}
