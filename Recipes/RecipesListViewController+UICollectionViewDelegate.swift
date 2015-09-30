fd0a3ce3ef51555d9c70c445780c5435694031df
nofelmahmood <nofelmehmood@gmail.com>
2015-09-30 13:28:07 +0500

    Adding RecipesListViewController collectionview UICollectionViewDelegate

---
+++ Recipes/RecipesListViewController+UICollectionViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  RecipesListViewController+UICollectionViewDelegate.swift
+//  Recipes
+//
+//  Created by Nofel Mahmood on 29/09/2015.
+//  Copyright © 2015 Hyper. All rights reserved.
+//
+
+import UIKit
+
+extension RecipesListViewController: UICollectionViewDelegate {
+  
+}
