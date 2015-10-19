5f125700a94755dedff7fe1956f656bea2a702b2
nofelmahmood <nofelmehmood@gmail.com>
2015-10-19 10:18:20 +0500

    Adding RecipeSearchTableViewCell

---
+++ Recipes/RecipeSearchTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeSearchTableViewCell.swift
+//  Recipes
+//
+//  Created by Nofel Mahmood on 01/10/2015.
+//  Copyright © 2015 Hyper. All rights reserved.
+//
+
+import UIKit
+
+class RecipeSearchTableViewCell: UITableViewCell {
+  
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var photoImageView: UIImageView!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+  override func prepareForReuse() {
+    self.nameLabel.text = ""
+    self.photoImageView.image = UIImage(named: "ImagePlaceholder")
+  }
+  
+}
