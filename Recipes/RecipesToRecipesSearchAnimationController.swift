f713d743219e672e15a7b3e98af6dfc02c3244a2
nofelmahmood <nofelmehmood@gmail.com>
2015-10-19 10:20:48 +0500

    Adding RecipesToRecipesSearchAnimationController

---
+++ Recipes/RecipesToRecipesSearchAnimationController.swift
@@ -0,0 +1,47 @@
+//
+//  RecipesToRecipesSearchAnimationController.swift
+//  Recipes
+//
+//  Created by Nofel Mahmood on 07/10/2015.
+//  Copyright © 2015 Hyper. All rights reserved.
+//
+
+import UIKit
+
+extension RecipesToRecipesSearchAnimationController: UIViewControllerAnimatedTransitioning {
+  func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+    return 1.0
+  }
+  
+  func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+    if (transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) as? UINavigationController)?.topViewController is RecipesSearchViewController {
+      let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+      let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+      let containerView = transitionContext.containerView()
+      (toViewController as? UINavigationController)?.setNavigationBarHidden(true, animated: true)
+      containerView?.addSubview(toViewController.view)
+      containerView?.sendSubviewToBack(toViewController.view)
+      UIView.animateWithDuration(transitionDuration(transitionContext), animations: {
+        (toViewController as? UINavigationController)?.setNavigationBarHidden(false, animated: true)
+        }, completion: { completion in
+          transitionContext.completeTransition(true)
+      })
+    } else {
+      let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+      let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+      let containerView = transitionContext.containerView()
+      containerView?.addSubview(toViewController.view)
+      containerView?.sendSubviewToBack(toViewController.view)
+      UIView.animateWithDuration(transitionDuration(transitionContext) - 0.4, animations: {
+        (fromViewController as? UINavigationController)?.setNavigationBarHidden(true, animated: true)
+        fromViewController.view.alpha = 0.0
+        }, completion: { completion in
+          transitionContext.completeTransition(true)
+      })
+    }
+  }
+}
+
+class RecipesToRecipesSearchAnimationController: NSObject {
+
+}
