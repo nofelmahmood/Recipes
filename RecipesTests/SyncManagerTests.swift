bd9d5e0e8bdba2f3fcf05d09814df0ce5c92478e
nofelmahmood <nofelmehmood@gmail.com>
2015-10-28 14:18:13 +0500

    Adding some tests to SyncManagerTests

--- RecipesTests/SyncManagerTests.swift
+++ RecipesTests/SyncManagerTests.swift
@@ -7,29 +7,43 @@
 //
 
 import XCTest
+import CoreData
+@testable import Recipes
 
 class SyncManagerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+  
+  func testSyncManager() {
+    let expectation = self.expectationWithDescription("SyncManager")
+    SyncManager.sharedManager.perform({
+      expectation.fulfill()
+    })
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    let expectation = self.expectationWithDescription("SyncManager.perform")
+    self.waitForExpectationsWithTimeout(5.0, handler: nil)
+    self.measureBlock {
+      // Put the code you want to measure the time of here.
+      SyncManager.sharedManager.perform({
+        expectation.fulfill()
+      })
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
